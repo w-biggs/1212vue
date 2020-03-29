00a8e58b5cabe38450895dfaecc13b6f50b1d4df
W Biggs <wilson@wilsonbiggs.com>
2020-03-29 15:30:10 -0400

    List confs alphabetically

--- store/confs.js
+++ store/confs.js
@@ -10,6 +10,7 @@ export const actions = {
     const useRemote = process.browser && (process.env.NODE_ENV === 'production');
     const apiLink = useRemote ? 'https://api.1212.one' : 'http://localhost:12121';
     const confs = await this.$axios.$get(`${apiLink}/confs/`);
+    confs.sort((a, b) => a.shortName.localeCompare(b.shortName));
     await commit('set', confs);
   },
 };
